c1f30853b7f759eab4e6f9b37032f55272dfde4c
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-11 16:05:23 +0200

    feat: "migration" structure created

---
+++ database/migrations/2024_06_11_132900_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 15);
+            $table->unsignedTinyInteger('number_carriages');
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('deleted')->default(0);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
